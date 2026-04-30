57a1c624dc5e22893dbb822b66ae559fd112793e
eurekanote <47346886+eurekanote@users.noreply.github.com>
2026-05-01 00:51:14 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'xiaoming-calc-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/5111/5111166.png'
+];
+
+// 安装时缓存所有资源
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// 离线时拦截请求，直接从缓存取
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
